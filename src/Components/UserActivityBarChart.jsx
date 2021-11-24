0f4dff10e784d0736c235a84b04a627444d54047
Raphael Pazdzior <rpazdzior@ceri-informatique.fr>
2021-11-24 17:25:20 +0100

    feat : add tooltip, Legend and ResponsiveContainer ( from recharts) on BarChart's activity

--- src/Components/UserActivityBarChart.jsx
+++ src/Components/UserActivityBarChart.jsx
@@ -1,17 +1,17 @@
 /**
  * return template with user's activity chart
  * used in MainBloc.jsx
- * tmp comment : help on recharts : https://www.geeksforgeeks.org/create-a-radar-chart-using-recharts-in-reactjs/
+ * tmp comment : help on recharts : https://www.geeksforgeeks.org/create-a-radar-chart-using-recharts-in-reactjs/ & https://recharts.org/en-US/api/BarChart
  * @param { array } props.datasActivity
  * @return { HTMLElement }
- *  datasActivity est un tabelau d'objet contenant :
+ *  datasActivity is an object array who contain for example :
  *  0: Object { day: "2022-07-01", kilogram: 8, calories: 240 }    ​
  *  1: Object { day: "2022-07-02", kilogram: 80, calories: 220 }
  */
  
 
 import PropTypes from 'prop-types';
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, Legend } from 'recharts';
 
 export default function UserActivityBarChart(props) {
     const sessions = props.datasActivity;
@@ -21,67 +21,73 @@ export default function UserActivityBarChart(props) {
     //console.log(sessions)      
     let arrayKilo = sessions.map(el => el.kilogram);
     let arrayCalories = sessions.map(el => el.calories);
-    let minKilo = Math.min(...arrayKilo); // opérateur de décomposition (spread opérator) car Math.min attent une suite de ciffre en argument (ex : 1 ,8, 0)
+    let minKilo = Math.min(...arrayKilo); // opérateur de décomposition (spread opérator) because  Math.min whaiting for a suit of number in argument (ex : 1 ,8, 0)
     let maxKilo = Math.max(...arrayKilo);
     let minCalories = Math.min(...arrayCalories);
     let maxCalories = Math.max(...arrayCalories) ;
     
     return <article className="user-page__graph__left__activity-chart">
         <h2 className="user-page__graph__left__activity-chart__title">Activité quotidienne</h2>
-
+            <ResponsiveContainer width="100%" height="100%">
             <BarChart
               data={sessions}
               margin={{
                 top: 100,
-                right: 0,
                 left: 20,
                 bottom: 20,
               }}
               barGap={10}
               barSize={10}
               width={850} height={250}
-            >
+            > {/*  width={850} heigh : no effect when ResponsiveContainerused */ }
 
-            <CartesianGrid strokeDasharray="3 3" vertical={false} /> 
+            <CartesianGrid strokeDasharray="3 3" vertical={false} /> {/* strokeDasharray  pattern of dashes and gaps used to paint the lines of the grid = lignes du fond dans graph */ }
               <XAxis
                 dataKey="day"
-                tickLine={false}
-                tick={{ fontSize: 12, strokeWidth: 0.5, stroke: 'grey' }}
+                tickLine={false} 
+                tick={{ fontSize: 12, strokeWidth: 0.5, stroke: '#C4C4C4' }}
                 dy={15}
-              />
+              />{/* tickLine = marqueur juste on top of the tick (tick = caption) */ }
 
               <YAxis
-                yAxisId="kilo"
+                yAxisId="kiloBar"
                 orientation="right"
                 interval="number"
                 allowDecimals={false}
                 tickLine={false}
                 axisLine={false}
                 hide={false}
-                tick={{ fontSize: 14 }}
+                tick={{ fontSize: 12, strokeWidth: 0.5, stroke: '#C4C4C4' }}
                 domain={[minKilo, maxKilo]}
               />
               <YAxis
-                yAxisId="cal"
+                yAxisId="calBar"
                 hide={true}
                 domain={[minCalories, maxCalories]}
-              />
+              /> {/* hide={true} becaus figma no show this info */ }
 
-
+            <Tooltip
+              cursor={{ fill: "#C4C4C4" }}
+            />   {/* fill : fond de la zone */} 
+  
+            <Legend verticalAlign="top" hei ght={36}
+            /> {/** could be perosnalised, see https://recharts.org/en-US/api/Legend */} 
               <Bar
-                yAxisId="kilo"
+                 name="Poids (Kg)"
+                yAxisId="kiloBar"
                 dataKey="kilogram"
                 radius={[50, 50, 0, 0]}
                 fill="#000"
               />
               <Bar
-                yAxisId="cal"
+                 name="Calories brûlées"
+                yAxisId="calBar"
                 dataKey="calories"
-                fill="#e60000"
+                fill="#E60000"
                 radius={[50, 50, 0, 0]}
               />
           </BarChart>
-
+          </ResponsiveContainer>
       </article>
 }
 
